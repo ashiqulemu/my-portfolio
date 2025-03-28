b0304c60572e7cacba3bc6ed6a40f956a91a7414
ashiqulemu <ashiqulemu.jpi@gmail.com>
2025-03-28 16:44:31 +0600

    ergregre

--- mailer.php
+++ mailer.php
@@ -9,34 +9,25 @@ require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
 //Load Composer's autoloader
-// require 'vendor/autoload.php';
+ require 'vendor/autoload.php'; 
 
 //Create an instance; passing `true` enables exceptions
 
+$mail = new PHPMailer(true);
+
 
 if (isset($_POST['submit'])) {
-    $mail = new PHPMailer(true);
+    
     try {
         //Server settings
-        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host = 'tls://smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                   //Enable SMTP authentication
         $mail->Username = 'ashiqulemu.jpi@gmail.com';                     //SMTP username
         $mail->Password = 'obshoajjtkpgerba';                               //SMTP password
-        // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->SMTPSecure = "tls";
-        $mail->Port = 587;  
-        
-        $mail->SMTPOptions = array(
-            'ssl' => array(
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-                'allow_self_signed' => true
-            )
-        );
-        
-        //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->SMTPSecure = false;            //Enable implicit TLS encryption
+        $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
 
@@ -88,7 +79,7 @@ if (isset($_POST['submit'])) {
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         $mail->send();
-        echo 'Message has been sent';
+        echo 'Message has been sent'; 
         header('location:thankyou.php');
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
