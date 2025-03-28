efdb7c928b717305f103c34d4fe73e9831e11d2e
ashiqulemu <ashiqulemu.jpi@gmail.com>
2025-03-28 23:53:20 +0600

    dsfsdf

--- mailer.php
+++ mailer.php
@@ -77,7 +77,7 @@ if (isset($_POST['submit'])) {
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         $mail->send();
-        echo 'Message has been sent'; 
+        // echo 'Message has been sent'; 
         header('location:thankyou.php');
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
